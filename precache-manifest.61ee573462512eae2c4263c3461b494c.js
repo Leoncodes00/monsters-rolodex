cb6a2a7372366d6e505f079a372fe7ce4eb22adc
Leoncodes00 <chachaiyang3@gmail.com>
2019-11-25 09:45:56 -0800

    Updates

---
+++ precache-manifest.61ee573462512eae2c4263c3461b494c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98725a2cc8475a7aed86b4dcddd92d1c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5dc2857f0c9e7f41aee0",
+    "url": "/monsters-rolodex/static/css/main.438c5276.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "5dc2857f0c9e7f41aee0",
+    "url": "/monsters-rolodex/static/js/main.06750bbd.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
